67e86dad8e2979fe3d1b4c49f08a6f6631d64037
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2017-06-15 13:44:03 +0800

    analysis of output

--- analysis.py
+++ analysis.py
@@ -1,12 +1,14 @@
+#-*- coding: UTF-8 -*-
 import sys
 f = open(sys.argv[1],'r')
 Tdict = dict()
 Tlist = list()
 condition = dict()
 
-condition["rate"] = 4.5
-condition["discount"] = 0.6
-condition["price"] = 200
+condition["rate"] = 3.5
+condition["discount"] = 0.7
+condition["price"] = 300
+condition["address"] = "新北市"
 
 while True:
     line = f.readline()
@@ -18,16 +20,18 @@ while True:
         continue
     elif line == "}": # End
         try:
-            if float(Tdict["rate"]) >= condition["rate"] and float(Tdict["discount"]) <= condition["discount"] and int(Tdict["price"]) <= condition["price"]:
+            if float(Tdict["rate"]) >= condition["rate"] and float(Tdict["discount"]) <= condition["discount"] and int(Tdict["price"]) <= condition["price"] and condition["address"] not in Tdict["address"]:
                 print "{"
                 for x in Tlist:
                     print x
                 print "}"
-        except:
+        except Exception,e: 
+            #print str(e)
             pass
     Tlist.append(line)
     tmp = line.split(" : ")
     if tmp[0] == "rate":
+        #print tmp[1]
         Tdict["rate"] = tmp[1]
     elif tmp[0] == "discount":
         Tdict["discount"] = tmp[1]
