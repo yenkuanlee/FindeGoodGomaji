2aa608327f409de1c05c43294e8a14879cbcd397
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2017-09-26 11:18:27 +0800

    update index 2017/09/26

--- crawler.py
+++ crawler.py
@@ -134,7 +134,7 @@ def Crawler(info):
 
 	Rdict['Sdict'] = GetScore(page_source)
 
-        if int(Rdict['price']) > 600:return
+        if int(Rdict['price']) > 800:return
         #if float(Rdict['price']) / float(Rdict['orign_price']) > 0.9:
         #        return
 
