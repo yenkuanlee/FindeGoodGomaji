267dd180467443df2b60cca9551e50d8f0d866f7
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2017-09-27 11:54:24 +0800

    update 2017/09/27

--- run_script.py
+++ run_script.py
@@ -1,10 +1,17 @@
 import os
+import time
 
+# Config
 api_key = ""    # Google api key to get rate    <---------------------------
 send_email = True   # Send new item to someone's email  <---------------------------
+keep_alive = True   # Keep alive or not <---------------------------
+period = 3600       # keep_alive period in Seconds  <---------------------------
+
+# Initialize
 user = "USER"   # Initial
 pwd = "PWD" # Initial
 email_send_to = "NOBODY"    # Initial
+
 if send_email:
     email_send_to = "" # Enter the email target <---------------------------
     import getpass
@@ -12,7 +19,11 @@ if send_email:
     pwd = getpass.getpass()
 
 
-
-os.system("python GetInfo.py > info.txt")
-os.system("python GetTaipeiInfo.py info.txt > info2.txt")
-os.system("python crawler.py info2.txt "+api_key+" "+email_send_to+" "+user+" "+pwd)
+while True:
+    os.system("python GetInfo.py > info.txt")
+    os.system("python GetTaipeiInfo.py info.txt > info2.txt")
+    os.system("python crawler.py info2.txt "+api_key+" "+email_send_to+" "+user+" "+pwd)
+    if keep_alive:
+        time.sleep(period) # Once per hour
+    else:
+        exit(0) # Just run one time
